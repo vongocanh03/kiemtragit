3b993dc19fc5b89d08ee45395de0c9aa8b9f0c66
vongocanh <anhhung05032003@gmail.com>
2024-10-21 18:01:27 +0700

    sua tong thanh ba so

--- Kiemtra.cpp
+++ Kiemtra.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    int a, b, sum;
+    int a, b, c, sum;
 
     // Nhap hai so nguyên
     printf("Nhap so thu nhat: ");
@@ -9,12 +9,14 @@ int main() {
 
     printf("Nhap so thu hai: ");
     scanf("%d", &b);
+        printf("Nhap so thu ba: ");
+    scanf("%d", &c);
 
     // Thuc hien phép cong
-    sum = a + b;
+    sum = a + b +c;
 
     // In ra ket qua
-    printf("Tong cua %d và %d là: %d\n", a, b, sum);
+    printf("Tong cua %d và %d và %d là: %d\n", a, b, c, sum);
 
     return 0;
 }
